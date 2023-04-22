cd019891845d7bc122527ab26b8ed420df68bf75
premmm123 <poomsakppk@gmail.com>
2023-04-22 18:51:59 +0700

    change image

--- back/crawler.py
+++ back/crawler.py
@@ -30,6 +30,3 @@ def createFile(fileName, url):
         writer = csv.writer(f)
         for item in data:
             writer.writerow([item])
-
-
-getData("https://th.wikipedia.org/wiki/%E0%B8%A3%E0%B8%B2%E0%B8%A2%E0%B8%8A%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B8%A7%E0%B8%B1%E0%B8%94%E0%B9%83%E0%B8%99%E0%B8%88%E0%B8%B1%E0%B8%87%E0%B8%AB%E0%B8%A7%E0%B8%B1%E0%B8%94%E0%B8%95%E0%B8%B2%E0%B8%81")
